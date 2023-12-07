e5f07a5694e788e9737fc717861cb2f9105fe1e5
Kamil Graczyk <kamil.graczyk@payback.net>
2023-12-07 17:23:54 +0100

    1st attempt

--- 2023/05/a.py
+++ 2023/05/a.py
@@ -1,55 +1,63 @@
 from input_file import text_input
-from collections import OrderedDict
 
-text_input="""seeds: 79 14 55 13
+# text_input="""seeds: 79 14 55 13
 
-seed-to-soil map:
-50 98 2
-52 50 48
+# seed-to-soil map:
+# 50 98 2
+# 52 50 48
 
-soil-to-fertilizer map:
-0 15 37
-37 52 2
-39 0 15
+# soil-to-fertilizer map:
+# 0 15 37
+# 37 52 2
+# 39 0 15
 
-fertilizer-to-water map:
-49 53 8
-0 11 42
-42 0 7
-57 7 4
+# fertilizer-to-water map:
+# 49 53 8
+# 0 11 42
+# 42 0 7
+# 57 7 4
 
-water-to-light map:
-88 18 7
-18 25 70
+# water-to-light map:
+# 88 18 7
+# 18 25 70
 
-light-to-temperature map:
-45 77 23
-81 45 19
-68 64 13
+# light-to-temperature map:
+# 45 77 23
+# 81 45 19
+# 68 64 13
 
-temperature-to-humidity map:
-0 69 1
-1 0 69
+# temperature-to-humidity map:
+# 0 69 1
+# 1 0 69
 
-humidity-to-location map:
-60 56 37
-56 93 4"""
+# humidity-to-location map:
+# 60 56 37
+# 56 93 4"""
 
 seeds = [int(seed) for seed in text_input.split("\n")[0].split(":")[1].split()]
-paths = []
 
 for line in text_input.split("\n")[2:]:
     if "map" in line:
-        mapping_dict = OrderedDict()
+        mapping_dict = {}
         continue
 
     if not line:
-        paths.append(mapping_dict)
+        new_seeds = []
+        while seeds:
+            current_seed = seeds.pop(0)
+            for r, o in mapping_dict.items():
+                bot, top = map(int, r.split("-"))
+                if bot <= current_seed <= top:
+                    new_seeds.append(current_seed + o)
+                    break
+            else:
+                new_seeds.append(current_seed)
+        seeds = new_seeds
         continue
+
 
     dest, rang, amnt = map(int, line.split())
     mapping_dict[f"{rang}-{rang + amnt}"] = dest - rang
-
 
 # idx = 0
 # temp
@@ -59,4 +67,4 @@ for line in text_input.split("\n")[2:]:
 #     else:
 #         seeds = [seed for seed in seeds if ]
 
-print(paths)
+print(min(seeds))
